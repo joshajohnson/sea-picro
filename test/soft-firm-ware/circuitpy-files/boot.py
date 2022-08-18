9fa8c87fbf1862ec7173769411c47f7a4a4ea592
Josh Johnson <josh@joshajohnson.com>
2022-08-18 20:49:19 +1000

    update cpy test code

---
+++ test/soft-firm-ware/circuitpy-files/boot.py
@@ -0,0 +1,2 @@
+import usb_cdc
+usb_cdc.enable(console=True, data=True)    # Enable console and data
--- test/soft-firm-ware/circuitpy-files/code.py
+++ test/soft-firm-ware/circuitpy-files/code.py
@@ -1,50 +1,114 @@
-import time
-
+# General Imports  
 import board
 import digitalio
+import time
+
+# Neopixel LED
 import neopixel
 from rainbowio import colorwheel
-import usb_hid
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode
 
-# Pinout order is around the normal pro micro pins anticlockwise, then the Elite-C pins anticlockwise. Maps to A->Z
-keypress_pins = [board.D0,  board.D1,   board.D2,   board.D3,   board.D4,   board.D5,   board.D6,   board.D7,   board.D8,   board.D9,   board.D21, board.D23, board.D20, board.D22, board.D26, board.D27, board.D28, board.D29, board.D12,  board.D13,  board.D14,  board.D15,  board.D16]
-keys_pressed =  [Keycode.A, Keycode.B,  Keycode.C,  Keycode.D,  Keycode.E,  Keycode.F,  Keycode.G,  Keycode.H,  Keycode.I,  Keycode.J,  Keycode.K, Keycode.L, Keycode.M, Keycode.N, Keycode.O, Keycode.P, Keycode.Q, Keycode.R, Keycode.S,  Keycode.T,  Keycode.U,  Keycode.V,  Keycode.W]
+# USB Serial
+import usb_cdc
+serial = usb_cdc.data
+if serial is None:
+    print("Need to enable USB CDC serial data in boot.py.")
+    while True:
+        pass
 
-key_pin_array = []
+# Function to print to CDC data not CDC REPL
+def serial_print(string):
+    serial.write("{}\r\n".format(string).encode())
 
-keyboard = Keyboard(usb_hid.devices)
-
-# Make all pins inputs with pullups
-for pin in keypress_pins:
-    key_pin = digitalio.DigitalInOut(pin)
-    key_pin.direction = digitalio.Direction.INPUT
-    key_pin.pull = digitalio.Pull.UP
-    key_pin_array.append(key_pin)
-
-# Init rainbow LED whilst we wait for testing
+# Init LED
 led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 led.brightness = 0.3
 j = 0
 
+# LED states for testing
+new_led_state = 0
+curr_led_state = 0
+LED_OFF = 0
+LED_RED = 1
+LED_GRN = 2
+LED_BLU = 3
+LED_RGB = 4
+
+# Romac test only, tie pins LOW - will be pulled to GND in actual test fixture
+low_pins = [board.D4, board.D5, board.D6, board.D7]
+for pin in low_pins:
+    key_pin = digitalio.DigitalInOut(pin)
+    key_pin.direction = digitalio.Direction.OUTPUT 
+    key_pin.value = False
+
+# IO Pins to be tested
+io_test_pins_init = [board.D26, board.D22, board.D20]
+io_test_pins_label = ['D26', 'D22', 'D20']
+# Array to store configured pins
+io_test_pins = []
+
+# VBUS detect pin
+vbus_pin = digitalio.DigitalInOut(board.D19)
+vbus_pin.direction = digitalio.Direction.INPUT
+vbus_pin.pull = digitalio.Pull.DOWN
+
+# Make all test pins inputs with pullups
+for pin in io_test_pins_init :
+    key_pin = digitalio.DigitalInOut(pin)
+    key_pin.direction = digitalio.Direction.INPUT
+    key_pin.pull = digitalio.Pull.UP
+    io_test_pins.append(key_pin)
+
 while True:
-    # Check each pin
-    for key_pin in key_pin_array:
-        if not key_pin.value:               # Check if pin is grounded
-            i = key_pin_array.index(key_pin)
-
-            while not key_pin.value:        # Wait for it to be ungrounded
+    # Check IO pins
+    for pin in io_test_pins:
+        if not pin.value:               # Check if pin is grounded
+            while not pin.value:        # Wait for it to be released
                 pass
-            keyboard.send(keys_pressed[i])  # With key released, send key's keycode
-            keyboard.send(Keycode.ENTER)    # Hit enter so the test script process the keypress
-            time.sleep(0.01)                # Hold key for 10ms
-            keyboard.release_all()          # Release all keys
+            time.sleep(0.1) # 100ms debounce
+            # TX on serial pin number per lookup table
+            serial_print(io_test_pins_label[io_test_pins.index(pin)])
 
-    # Fade LED colours whilst we test
-    j = (j + 1) % 256
-    led.fill(colorwheel(j))
-    time.sleep(0.01)
+    # Check if we have a message waiting for us on UART
+    if serial.in_waiting > 0:
+        rx_msg = serial.readline().decode()[0:-2] # Convert from byte array and strip \r\n
 
+        if rx_msg == "ident":
+            serial_print("ident = Sea-Picro!")
 
+        elif rx_msg == "vbus_test":
+            vbus_state = vbus_pin.value
+            if (vbus_state):
+                serial_print("vbus = high")
+            else:
+                serial_print("vbus = low") 
+
+        elif rx_msg == "led_off":
+            new_led_state = LED_OFF
+        elif rx_msg == "led_red":
+            new_led_state = LED_RED
+        elif rx_msg == "led_grn":
+            new_led_state = LED_GRN
+        elif rx_msg == "led_blu":
+            new_led_state = LED_BLU
+        elif rx_msg == "led_rgb":
+            new_led_state = LED_RGB
+
+        else:
+            print("Unknown RX MSG: " + rx_msg)
+
+    # Set LED state
+    if new_led_state != curr_led_state or new_led_state == LED_RGB or curr_led_state == LED_RGB:
+        curr_led_state = new_led_state
+
+        if curr_led_state == LED_OFF:
+            led.fill((0,0,0))
+        elif curr_led_state == LED_RED:
+            led.fill((255,0,0))
+        elif curr_led_state == LED_GRN:
+            led.fill((0,255,0))
+        elif curr_led_state == LED_BLU:
+            led.fill((0,0,255))
+        elif curr_led_state == LED_RGB:
+            j = (j + 1) % 256 
+            led.fill(colorwheel(j))
+            time.sleep(0.01)
